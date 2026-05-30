9fa107a73928cfff8eb6ca177057a576fd220d0e
gtifaro <gabrieltifaro2021@gmail.com>
2026-05-29 23:01:11 -0500

    feat: Add create view with form

--- frontend/src/view/classes/classView.jsx
+++ frontend/src/view/classes/classView.jsx
@@ -1,19 +1,22 @@
-import {Link} from "react-router-dom";
-import {useClasses} from "../../controller/useClasses";
+import { Link } from "react-router-dom";
+import { useClasses } from "../../controller/useClasses";
 
 export function ClassView() {
   const { classes, loading } = useClasses();
   if (loading) return <p>Cargando clases...</p>;
   return (
-    <ul>
-      {classes &&
-        classes.map((c) => {
-          return (
-            <li key={c._id}>
-              {c.test} <Link to={`/classes/edit/${c._id}`}>Editar</Link>
-            </li>
-          );
-        })}
-    </ul>
+    <>
+      <Link to="/classes/create">Crear nueva clase</Link>
+      <ul>
+        {classes &&
+          classes.map((c) => {
+            return (
+              <li key={c._id}>
+                {c.test} <Link to={`/classes/edit/${c._id}`}>Editar</Link>
+              </li>
+            );
+          })}
+      </ul>
+    </>
   );
 }
--- frontend/src/view/classes/create/CreateClassView.jsx
+++ frontend/src/view/classes/create/CreateClassView.jsx
@@ -1,1 +1,20 @@
-export function CreateClassView() {}
+export function CreateClassView() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h1>Crear clase</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Test:
+          <input type="text" name="test" placeholder="M1l" />
+        </label>
+        <button type="submit">Crear</button>
+      </form>
+    </>
+  );
+}
